89f1368079254100b2e64add44b5f43986fdd489
wanganran <wanganran01@gmail.com>
2021-03-12 18:39:38 -0800

    update

--- model.py
+++ model.py
@@ -43,33 +43,42 @@ def toReal(tensor):
     return tensor[:,0,...]
 
 class CausalComplexConv1d(nn.Module):
-    def __init__(self, channel_in, channel_out, kernel_size, stride=1, dilation=1):
+    def __init__(self, channel_in, channel_out, kernel_size, stride=1, dilation=1, fullconv=False):
         super().__init__()
         # input is [batch, channel, length]
         # depthwise + 1x1
         self.kernel_size=kernel_size
         self.stride=stride
         self.pad=nn.ConstantPad1d((kernel_size*dilation-dilation+1-stride,0), 0)
-        self.l1=ComplexConv1d(channel_in, channel_in, kernel_size, stride=stride, dilation=dilation, groups=channel_in, padding=0)
-        #self.l1=ComplexConv1d(channel_in, channel_in, kernel_size, stride=stride, dilation=dilation, groups=channel_in, padding=kernel_size*dilation-dilation+1-stride)
-        self.l2=ComplexConv1d(channel_in, channel_out, 1)
-    
+        self.fullconv=fullconv
+        if not fullconv:
+            self.l1=ComplexConv1d(channel_in, channel_in, kernel_size, stride=stride, dilation=dilation, groups=channel_in, padding=0)
+            self.l2=ComplexConv1d(channel_in, channel_out, 1)
+        else:
+            self.l=ComplexConv1d(channel_in, channel_out, kernel_size, stride=stride, dilation=dilation, padding=0)
     def forward(self, x):
-        #return self.l2(cSymRelu6(self.l1(x)[..., :(-self.kernel_size//self.stride+1)]))
-        return self.l2(cSymRelu6(self.l1(self.pad(x))))
-        
+        if not self.fullconv:
+            return self.l2(cSymRelu6(self.l1(self.pad(x))))
+        else:
+            return self.l(self.pad(x))
 
 class CausalComplexConvTrans1d(nn.Module):
-    def __init__(self, channel_in, channel_out, kernel_size, stride=1):
+    def __init__(self, channel_in, channel_out, kernel_size, stride=1, fullconv=False):
         super().__init__()
         self.kernel_size=kernel_size
         self.stride=stride
-        self.l1=ComplexConv1d(channel_in, channel_out, 1)
-        self.l2=ComplexConvTranspose1d(channel_out, channel_out, kernel_size, stride=stride, groups=channel_out)
-    
+        self.fullconv=fullconv
+        if not fullconv:
+            self.l1=ComplexConv1d(channel_in, channel_out, 1)
+            self.l2=ComplexConvTranspose1d(channel_out, channel_out, kernel_size, stride=stride, groups=channel_out)
+        else:
+            self.l=ComplexConvTranspose1d(channel_in, channel_out, kernel_size, stride=stride)
+            
     def forward(self, x):
-        return self.l2(cSymRelu6(self.l1(x)))[..., self.kernel_size-self.stride:]
-
+        if not self.fullconv:
+            return self.l2(cSymRelu6(self.l1(x)))[..., self.kernel_size-self.stride:]
+        else:
+            return self.l(x)[..., self.kernel_size-self.stride:]
     
 class SimpleNetwork(nn.Module):        
     def __init__(self, in_channels, channels, kernel_size, reception, lstm_layers=2, depth=2, context=3, stride=2):
@@ -93,43 +102,48 @@ class SimpleNetwork(nn.Module):
         self.wconvs=nn.ModuleList()
         dilation=1
         for i in range(wnet_layers):
-            self.wconvs.append(CausalComplexConv1d(channels, channels, wnet_reception, stride=1, dilation=dilation))
+            self.wconvs.append(CausalComplexConv1d(channels, channels, wnet_reception, stride=1, dilation=dilation, fullconv=True))
             self.wconvs.append(CSymRelu6())
             dilation*=wnet_reception
         
+        self.first=CausalComplexConv1d(in_channels, channels, reception)
+        self.last=CausalComplexConvTrans1d(channels*2, 1, reception, fullconv=True)
         for i in range(depth):
             encode=nn.ModuleDict()
-            encode["conv"]=CausalComplexConv1d(channels, channels, kernel_size, stride=stride) if i>0 else CausalComplexConv1d(in_channels, channels, reception)
+            encode["conv"]=CausalComplexConv1d(channels, channels, kernel_size, stride=stride, fullconv=True)
             encode["relu"]=CSymRelu6()
             
             self.encoders.append(encode)
             
             decode=nn.ModuleDict()
-            decode["conv"]=CausalComplexConvTrans1d(channels*2, channels, kernel_size, stride=stride) if i!=depth-1 else CausalComplexConvTrans1d(channels*2, 1, reception)
-            if i<depth-1:
-                decode["relu"]=CSymRelu6()
+            decode["conv"]=CausalComplexConvTrans1d(channels*2, channels, kernel_size, stride=stride) 
             self.decoders.append(decode)
 
     def forward(self, mix:torch.Tensor):
         # mix: [batch, channel, length]
         # mix=torch.view_as_complex(torch.stack((mix, mix-mix), dim=-1))
         mix=toComplex(mix)
-        saved=[mix]
-        x=mix
+        x=self.first(mix)
+        
+        # attention
+        x2=x # B, channels, L
+        
+        for layer in self.wconvs:
+            x2=layer(x2)
+        
+        x=x*x2
+        
+        saved=[x]
+        
+        # encoders
         id=0
         for encode in self.encoders:
             x=encode["conv"](x)
             x=encode["relu"](x)
             saved.append(x)
-            #print(x.shape)
             id+=1
         
-        x2=x
-        for layer in self.wconvs:
-            x=layer(x)
-            #print(x.shape)
         
-        x=x+x2
         
         '''
         x=toReal(x)
@@ -138,16 +152,19 @@ class SimpleNetwork(nn.Module):
         x = x.permute(1,2,0)
         x=toComplex(x)
         '''
-        
+        # decoders
         for decode in self.decoders:
             # skip=center_trim(saved.pop(-1), x)
             skip=saved.pop(-1)
             #x=x+skip
             x=torch.cat((x, skip), -2)
             x=decode["conv"](x)
-            if "relu" in decode:
-                x=decode["relu"](x)
-            # print(x.shape)
+        
+        # last layer
+        skip=saved.pop(-1)
+        x=torch.cat((x, skip), -2)
+        x=self.last(x)
+        
         return toReal(x)
     
     def loss(self, signal, gt, mix):
@@ -160,5 +177,5 @@ class SimpleNetwork(nn.Module):
         return (l1*0.1+l2*0.5+l3)/total_len
         '''
         #return si_sdr_loss(signal, gt)
-        #return F.l1_loss(signal[..., 24000:], gt[..., 24000:])
-        return util.wsdr_loss(mix, signal, gt)
+        return F.l1_loss(signal[..., 24000:], gt[..., 24000:])
+        #return util.wsdr_loss(mix, signal, gt)
